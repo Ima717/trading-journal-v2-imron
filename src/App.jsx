ddab0b649b9112d47076f5f25bc4cc1382b7561e
Ima717 <2006imron@gmail.com>
2025-03-25 17:49:14 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,31 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import AddTrade from "./pages/AddTrade";
 import PrivateRoute from "./components/PrivateRoute";
-import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route
-            path="/dashboard"
-            element={
-              <PrivateRoute>
-                <Dashboard />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/add-trade"
-            element={
-              <PrivateRoute>
-                <AddTrade />
-              </PrivateRoute>
-            }
-          />
-        </Routes>
-      </Router>
-    </AuthProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/add-trade"
+          element={
+            <PrivateRoute>
+              <AddTrade />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
