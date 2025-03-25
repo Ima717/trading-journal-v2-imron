01e8142358f380d0b0c6a4743f7ef59772f898a1
Ima717 <2006imron@gmail.com>
2025-03-25 23:56:14 +0100

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -24,7 +24,7 @@ const Dashboard = () => {
         <button
   onClick={handleLogout}
   className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded shadow transition duration-200 ease-in-out">
-  Log Out
+  Hello
         </button>
 
       </div>
