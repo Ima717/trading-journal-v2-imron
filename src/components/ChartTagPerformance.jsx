c4f4327f272ac67439c4c29d93bd8c61d7077e01
Ima717 <2006imron@gmail.com>
2025-03-25 20:38:45 +0100

    Update ChartTagPerformance.jsx

--- src/components/ChartTagPerformance.jsx
+++ src/components/ChartTagPerformance.jsx
@@ -1,17 +1,19 @@
+// src/components/ChartTagPerformance.jsx
+
 import React from "react";
 import {
   BarChart,
   Bar,
   XAxis,
   YAxis,
-  CartesianGrid,
   Tooltip,
   ResponsiveContainer,
+  CartesianGrid,
 } from "recharts";
 
 const ChartTagPerformance = ({ data, onTagClick }) => {
-  const handleBarClick = (data) => {
-    const tag = data.activeLabel;
+  const handleBarClick = (bar) => {
+    const tag = bar.activeLabel;
     if (tag && onTagClick) {
       onTagClick(tag);
     }
@@ -22,14 +24,20 @@ const ChartTagPerformance = ({ data, onTagClick }) => {
       <ResponsiveContainer width="100%" height="100%">
         <BarChart
           data={data}
-          margin={{ top: 20, right: 30, left: 20, bottom: 20 }}
+          layout="vertical"
+          margin={{ top: 10, right: 30, left: 60, bottom: 10 }}
           onClick={handleBarClick}
         >
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="tag" />
-          <YAxis />
+          <XAxis type="number" />
+          <YAxis dataKey="tag" type="category" />
           <Tooltip />
-          <Bar dataKey="avgPnL" fill="#7C3AED" radius={[4, 4, 0, 0]} />
+          <Bar
+            dataKey="avgPnL"
+            fill="#a855f7"
+            radius={[10, 10, 10, 10]}
+            isAnimationActive={true}
+          />
         </BarChart>
       </ResponsiveContainer>
     </div>
