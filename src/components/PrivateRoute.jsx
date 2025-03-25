0d51ad3667dd929d19c1c20d89b28432f0c9a338
Ima717 <2006imron@gmail.com>
2025-03-25 17:38:57 +0100

    Update PrivateRoute.jsx

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -6,7 +6,7 @@ const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
 
   if (loading) {
-    return <div className="text-center mt-10 text-lg">Loading...</div>;
+    return <div>Loading...</div>;
   }
 
   return user ? children : <Navigate to="/signin" />;
