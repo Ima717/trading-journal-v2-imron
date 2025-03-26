d4f4e6a0117892d484bf6ea9757fc7c48d85e80e
Ima717 <2006imron@gmail.com>
2025-03-26 22:30:34 +0100

    Update PerformanceChart.jsx

--- src/components/PerformanceChart.jsx
+++ src/components/PerformanceChart.jsx
@@ -10,24 +10,29 @@ import {
 
 const PerformanceChart = ({ data }) => {
   return (
-    <div className="bg-white dark:bg-zinc-900 shadow rounded-2xl p-4 h-72 my-6">
+    <div className="bg-white dark:bg-zinc-900 shadow rounded-2xl px-4 py-6 my-6">
       <h2 className="text-md font-medium mb-3 text-zinc-800 dark:text-white">
         📈 PnL Over Time
       </h2>
-      <ResponsiveContainer width="100%" height="100%">
-        <LineChart data={data}>
-          <XAxis dataKey="date" />
-          <YAxis />
-          <Tooltip />
-          <Line
-            type="monotone"
-            dataKey="pnl"
-            stroke="#4ade80"
-            strokeWidth={2}
-            dot={false}
-          />
-        </LineChart>
-      </ResponsiveContainer>
+      <div className="h-64">
+        <ResponsiveContainer width="100%" height="100%">
+          <LineChart
+            data={data}
+            margin={{ top: 10, right: 20, left: 0, bottom: 30 }}
+          >
+            <XAxis dataKey="date" tick={{ fontSize: 12 }} />
+            <YAxis tick={{ fontSize: 12 }} />
+            <Tooltip />
+            <Line
+              type="monotone"
+              dataKey="pnl"
+              stroke="#4ade80"
+              strokeWidth={2}
+              dot={false}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 };
