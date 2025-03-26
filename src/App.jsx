0421b9b08ffb143eaf8c584f640bbb5908517b27
Ima717 <2006imron@gmail.com>
2025-03-26 20:57:29 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,37 +9,40 @@ import PrivateRoute from "./components/PrivateRoute";
 import Test from './pages/Test';
 import ImportTrades from "./pages/ImportTrades";
 import CalendarPage from "./pages/CalendarPage";
+import { FilterProvider } from "./context/FilterContext";
 
 function App() {
   return (
     <Router>
       <AuthProvider>
-        <Routes>
-          {/* Public Routes */}
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/test" element={<Test />} />
-          <Route path="/import" element={<ImportTrades />} />
-          <Route path="/calendar" element={<CalendarPage />} />
+        <FilterProvider>
+          <Routes>
+            {/* Public Routes */}
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/test" element={<Test />} />
+            <Route path="/import" element={<ImportTrades />} />
+            <Route path="/calendar" element={<CalendarPage />} />
 
-          {/* Protected Routes */}
-          <Route
-            path="/"
-            element={
-              <PrivateRoute>
-                <Dashboard />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/add-trade"
-            element={
-              <PrivateRoute>
-                <AddTrade />
-              </PrivateRoute>
-            }
-          />
-        </Routes>
+            {/* Protected Routes */}
+            <Route
+              path="/"
+              element={
+                <PrivateRoute>
+                  <Dashboard />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/add-trade"
+              element={
+                <PrivateRoute>
+                  <AddTrade />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </FilterProvider>
       </AuthProvider>
     </Router>
   );
