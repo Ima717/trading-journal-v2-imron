de772f599d0fe2450900ec1bde384096b9eb8654
Ima717 <2006imron@gmail.com>
2025-04-01 13:24:48 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// /src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignIn from "./pages/SignIn";
@@ -6,13 +5,14 @@ import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
 import AddTrade from "./pages/AddTrade";
 import EditTrade from "./pages/EditTrade";
-import Trades from "./pages/Trades"; // Import the new Trades page
-import { AuthProvider } from "./context/AuthContext";
+import Trades from "./pages/Trades";
 import Test from "./pages/Test";
 import ImportTrades from "./pages/ImportTrades";
 import CalendarPage from "./pages/CalendarPage";
+import { AuthProvider } from "./context/AuthContext";
 import { FilterProvider } from "./context/FilterContext";
 import { ThemeProvider } from "./context/ThemeContext";
+import MainLayout from "./layouts/MainLayout"; // 👈 Import the layout
 
 function App() {
   return (
@@ -21,18 +21,20 @@ function App() {
         <FilterProvider>
           <ThemeProvider>
             <Routes>
-              {/* Public Routes */}
+              {/* Public routes */}
               <Route path="/signin" element={<SignIn />} />
               <Route path="/signup" element={<SignUp />} />
               <Route path="/test" element={<Test />} />
-              <Route path="/import" element={<ImportTrades />} />
-              <Route path="/calendar" element={<CalendarPage />} />
 
-              {/* Dashboard is now public for dev access */}
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/add-trade" element={<AddTrade />} />
-              <Route path="/edit-trade/:id" element={<EditTrade />} />
-              <Route path="/trades" element={<Trades />} /> {/* Add the new route */}
+              {/* Protected layout */}
+              <Route element={<MainLayout />}>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/add-trade" element={<AddTrade />} />
+                <Route path="/edit-trade/:id" element={<EditTrade />} />
+                <Route path="/trades" element={<Trades />} />
+                <Route path="/import" element={<ImportTrades />} />
+                <Route path="/calendar" element={<CalendarPage />} />
+              </Route>
             </Routes>
           </ThemeProvider>
         </FilterProvider>
