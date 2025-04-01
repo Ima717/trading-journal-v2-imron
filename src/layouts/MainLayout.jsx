9aeae6f016bc882a2e22b9d3f4d7882e9f499f94
Ima717 <2006imron@gmail.com>
2025-04-01 17:08:12 +0200

    Update MainLayout.jsx

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,13 +1,18 @@
-// MainLayout.jsx
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "../components/Sidebar";
 import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
     <div className="flex min-h-screen">
-      <Sidebar />
-      <div className="flex-1 ml-[240px] transition-all duration-300 bg-gray-100 dark:bg-zinc-900">
+      <Sidebar collapsed={collapsed} setCollapsed={setCollapsed} />
+      <div
+        className={`flex-1 transition-all duration-300 bg-gray-100 dark:bg-zinc-900 ${
+          collapsed ? "ml-[60px]" : "ml-[240px]"
+        }`}
+      >
         <Outlet />
       </div>
     </div>
