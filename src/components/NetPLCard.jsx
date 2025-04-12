51b4050121c6abc07ef12092d906a61f2245e06a
Ima717 <2006imron@gmail.com>
2025-04-12 13:11:15 +0200

    Update NetPLCard.jsx

--- src/components/NetPLCard.jsx
+++ src/components/NetPLCard.jsx
@@ -13,7 +13,7 @@ const NetPLCard = ({ value, badge, trades }) => {
   const color = value >= 0 ? "text-green-600" : "text-red-500";
 
   return (
-    <div className="p-6 rounded-xl shadow-sm bg-white dark:bg-zinc-800 min-w-[200px] max-w-[300px] h-24 flex flex-col justify-between hover:shadow-md hover:scale-[1.02] transition-all duration-200">
+    <div className="p-6 rounded-xl shadow-sm bg-white dark:bg-zinc-800 min-w-[200px] flex-1 h-24 flex flex-col justify-between hover:shadow-md hover:scale-[1.02] transition-all duration-200">
       <div className="flex justify-between items-center mb-1">
         <div className="flex items-center gap-1 text-xs text-gray-600 dark:text-gray-400 font-medium">
           Net P&L
